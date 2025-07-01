1545fdec2c260048f4e402c47c866f3cdadc2914
hoang1007 <hoangvuhuy1007@gmail.com>
2025-07-01 12:19:49 +0700

    string_utils: Add a method to validate text

--- src/pyvinorm/utils/string_utils.py
+++ src/pyvinorm/utils/string_utils.py
@@ -1,4 +1,6 @@
 import re
+import regex
+import string
 from pyvinorm.managers import Mapping
 
 
@@ -16,6 +18,20 @@ def replace_nonvoice_symbols(text: str, repl: str = "") -> str:
     # Define a regex pattern to match non-voice symbols
     pattern = re.compile(r"(“|”|\.\.\.|\"|\'|\{|\}|\[|\]|\(|\))")
     return pattern.sub(repl, text)
+
+
+VI_LETTERS = "aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ"
+
+
+def is_valid(text: str) -> bool:
+    """
+    Whether input text is a valid text.
+
+    A valid text is a text that does not empty and only contains
+    Vietnamese letters, digits, spaces, and some punctuation marks.
+    """
+    pattern = regex.compile(f"^[{VI_LETTERS}0-9\\s{regex.escape(string.punctuation)}]+$")
+    return (pattern.fullmatch(text) is not None) and (len(text) > 0)
 
 
 # def contain_only_letters(text: str, letter_mapping: Mapping) -> bool:
